7a9883ca0e842e33bd89367d17e36bf6280bd297
Dante Tagliavini <dftagliavini@indracompany.com>
2026-03-10 20:27:57 +0000

    wizard step2

--- ypf_ui5_hr_saldo/webapp/controller/View1.controller.js
+++ ypf_ui5_hr_saldo/webapp/controller/View1.controller.js
@@ -105,7 +105,175 @@ sap.ui.define([
         onPasoAnterior1: function () {
             var oWizard = this.byId("wizardId");
             var oStepActual = this.byId("PrimerStep");
-                oWizard.previousStep();
+            oWizard.previousStep();
+        },
+        onInformacionValidacion: function () {
+
+        },
+        onSiguientePaso2: function () {
+            var oWizard = this.byId("wizardId");
+            var oStepActual = this.byId("SegundoStep");
+
+            this.onInformacionValidacion();
+
+            if (oStepActual.getValidated()) {
+                oWizard.nextStep();
+            }
+        },
+        onInformacionValidacion: function () {
+            var oWizard = this.byId("wizardId");
+            var oStep2 = this.byId("SegundoStep");
+
+            var bCompleto =
+                !!this.byId("inpSucursal").getValue().trim() &&
+                !!this.byId("inpPedido").getValue().trim() &&
+                !!this.byId("inpHojaEntrada").getValue().trim() &&
+                !!this.byId("inpCentroCosto").getValue().trim() &&
+                !!this.byId("inpEjercicio").getValue().trim() &&
+                !!this.byId("cmbMoneda").getSelectedKey() &&
+                !!this.byId("inpImporte").getValue().trim();
+
+            if (bCompleto) {
+                oWizard.validateStep(oStep2);
+            } else {
+                oWizard.invalidateStep(oStep2);
+            }
+
+        },
+        onCancelarSolicitud: function () {
+
+            sap.m.MessageBox.confirm(
+                "¿Desea cancelar la solicitud?",
+                {
+                    actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+                    onClose: function (sAction) {
+
+                        if (sAction === sap.m.MessageBox.Action.YES) {
+
+                            sap.m.MessageToast.show("Solicitud cancelada");
+
+                            // ejemplo: limpiar wizard o volver
+                            var oWizard = this.byId("wizardId");
+                             this._resetFormulario();
+                            oWizard.discardProgress(oWizard.getSteps()[0]);
+                        }
+
+                    }.bind(this)
+                }
+            );
+
+        },
+        // =============================
+        // MATCHCODE PEDIDO
+        // =============================
+        onValueHelpPedido: function (oEvent) {
+
+            var oInput = oEvent.getSource();
+
+            var oDialog = new sap.m.SelectDialog({
+                title: "Seleccionar Pedido",
+                items: [
+                    new sap.m.StandardListItem({ title: "4500000010", description: "Pedido Materiales" }),
+                    new sap.m.StandardListItem({ title: "4500000020", description: "Pedido Servicios" }),
+                    new sap.m.StandardListItem({ title: "4500000030", description: "Pedido Mantenimiento" })
+                ],
+                confirm: function (oEvent) {
+                    var oItem = oEvent.getParameter("selectedItem");
+                    if (oItem) {
+                        oInput.setValue(oItem.getTitle());
+                        this.onInformacionValidacion();
+                    }
+                }
+            });
+
+            oDialog.open();
+        },
+
+
+        // =============================
+        // MATCHCODE CENTRO COSTO
+        // =============================
+        onValueHelpCentroCosto: function (oEvent) {
+
+            var oInput = oEvent.getSource();
+
+            var oDialog = new sap.m.SelectDialog({
+                title: "Seleccionar Centro de Costo",
+                items: [
+                    new sap.m.StandardListItem({ title: "CC1000", description: "Administración" }),
+                    new sap.m.StandardListItem({ title: "CC2000", description: "Operaciones" }),
+                    new sap.m.StandardListItem({ title: "CC3000", description: "Mantenimiento" })
+                ],
+                confirm: function (oEvent) {
+                    var oItem = oEvent.getParameter("selectedItem");
+                    if (oItem) {
+                        oInput.setValue(oItem.getTitle());
+                        this.onInformacionValidacion();
+                    }
+                }
+            });
+
+            oDialog.open();
+        },
+
+
+        // =============================
+        // MATCHCODE CENTRO BENEFICIO
+        // =============================
+        onValueHelpCentroBeneficio: function (oEvent) {
+
+            var oInput = oEvent.getSource();
+
+            var oDialog = new sap.m.SelectDialog({
+                title: "Seleccionar Centro de Beneficio",
+                items: [
+                    new sap.m.StandardListItem({ title: "CB1000", description: "Refinación" }),
+                    new sap.m.StandardListItem({ title: "CB2000", description: "Distribución" }),
+                    new sap.m.StandardListItem({ title: "CB3000", description: "Comercialización" })
+                ],
+                confirm: function (oEvent) {
+                    var oItem = oEvent.getParameter("selectedItem");
+                    if (oItem) {
+                        oInput.setValue(oItem.getTitle());
+                        this.onInformacionValidacion();
+                    }
+                }
+            });
+
+            oDialog.open();
+        },
+        _resetFormulario: function () {
+
+            var oWizard = this.byId("wizardId");
+
+            // Inputs Step 1
+            this.byId("inpSociedad").setValue("");
+            this.byId("inpProveedor").setValue("");
+
+            // Combo Step 1
+            this.byId("cmbTipoSolicitud").setSelectedKey("");
+            this.byId("cmbUbicacion").setSelectedKey("");
+
+            // Inputs Step 2
+            this.byId("inpSucursal").setValue("1101"); // valor default
+            this.byId("inpPedido").setValue("");
+            this.byId("inpHojaEntrada").setValue("");
+            this.byId("inpCentroCosto").setValue("");
+            this.byId("inpEjercicio").setValue("");
+            this.byId("inpImporte").setValue("");
+            this.byId("inpReferencia").setValue("");
+            this.byId("inpCentroBeneficio").setValue("");
+
+            // Combo Step 2
+            this.byId("cmbMoneda").setSelectedKey("");
+
+            // Reset Wizard
+            oWizard.discardProgress(oWizard.getSteps()[0]);
+
+            // Reset validaciones
+            oWizard.invalidateStep(this.byId("PrimerStep"));
+            oWizard.invalidateStep(this.byId("SegundoStep"));
+
         }
     });
 });
